6091df7ef2f050df0a5e02c2a9868f9b1480065e
Artemon-x58 <artem1145819@gmail.com>
2023-10-11 22:08:25 +0200

    type

---
+++ lesson 1/dist/index.js
@@ -0,0 +1,7 @@
+// function add(num1: number, num2: number): string {
+//   return num1 + num2;
+// }
+const user = {
+    name: "Bob",
+    age: 12,
+};
---
+++ lesson 1/src/index.ts
@@ -0,0 +1,32 @@
+// function add(num1: number, num2: number): string {
+//   return num1 + num2;
+// }
+
+// add("1", 1);
+// let total: number = 100;
+// let name: string = "Bob";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
+// let age = 10;
+// age = "12";
+
+// const numbers: number[] = [1, 2, 3, 4, 5, 6, 3, 0];
+// numbers.push(12);
+
+// const user: { name: string; age: number } = {
+//   name: "Bob",
+//   age: 12,
+// };
+
+// user.name = "Sem";
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const user: User = {
+  name: "Bob",
+  age: 12,
+};
